51b64975ec94c0dc60e24941d36cb0ec0100dea9
Duygu Tuncer Mullin <duygu.tuncer001@gmail.com>
2023-08-18 16:59:41 -0500

    feat(main) add shoppingCart, create new Shop class to centralize method calls.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,65 @@
 class Product {
-  title = "default";
+  title;
   imageUrl;
   price;
   description;
 
   constructor(title, imageUrl, price, description) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.description = description;
+    (this.title = title),
+      (this.imageUrl = imageUrl),
+      (this.price = price),
+      (this.description = description);
   }
 }
 
-const productList = {
-  products: [
+class ShoppingCart {
+  items = [];
+
+  render() {
+    const cartContainer = document.createElement("section");
+    cartContainer.className = "cart";
+    cartContainer.innerHTML = `
+          <h2>Total: ${0}</h2>
+          <button> Buy it now! </button>
+      `;
+    return cartContainer;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log("adding to the cart");
+    console.log("product", this.product);
+  }
+
+  render() {
+    const listItem = document.createElement("li");
+    listItem.className = "product-item";
+    listItem.innerHTML = `
+      <div>
+          <img src = "${this.product.imageUrl}" alt = ${this.product.title}/>
+          <div product-item__content>
+          <h1>${this.product.title}</h1>
+          <h3>\$${this.product.price}</h3> 
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+          </div>
+      </div>
+      `;
+
+    const button = listItem.querySelector("button");
+    button.addEventListener("click", this.addToCart.bind(this));
+
+    return listItem;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "pillow",
       "https://assets.pbimgs.com/pbimgs/ab/images/dp/wcm/202312/0693/belgian-linen-pillow-c.jpg",
@@ -27,30 +73,35 @@ const productList = {
       99.99,
       "a nice carpet"
     ),
-  ],
+  ];
 
   render() {
-    const mainRoot = document.getElementById("app");
+    // const root = document.getElementById("app");
     const ulEl = document.createElement("ul");
     ulEl.className = "product-list";
-    for (const product of this.products) {
-      const newLiEl = document.createElement("li");
-      newLiEl.className = "product-item";
-      newLiEl.innerHTML = `
-        <div>
-            <img src = "${product.imageUrl}" alt = ${product.title}/>
-            <div product-item__content>
-            <h1>${product.title}</h1>
-            <h3>\$${product.price}</h3> 
-            <p>${product.description}</p>
-            <button>Add to Cart</button>
-            </div>
-        </div>
-        `;
-      ulEl.append(newLiEl);
-    }
-    mainRoot.append(ulEl);
-  },
-};
 
-productList.render();
+    this.products.forEach((product) => {
+      const productItem = new ProductItem(product);
+      const renderedProductItem = productItem.render();
+      ulEl.append(renderedProductItem);
+    });
+    return ulEl;
+  }
+}
+
+class Shop {
+  render() {
+    const productList = new ProductList();
+    const productListEl = productList.render();
+
+    const shoppingCart = new ShoppingCart();
+    const cartEl = shoppingCart.render();
+
+    const root = document.getElementById("app");
+    root.append(cartEl);
+    root.append(productListEl);
+  }
+}
+
+const shop = new Shop();
+shop.render();
